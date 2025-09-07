9c53dc8122ac84a7a6ed5fc2ba126126796b2808
hexselarchieles <hexselavmm@gmail.com>
2025-09-07 13:52:27 +0700

    Initial commit

---
+++ app/src/main/java/com/example/lab_week_02_a/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.example.lab_week_02_a
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import android.util.Log
+
+
+
+class MainActivity : AppCompatActivity() {
+    companion object {
+        private const val TAG = "MainActivity"
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        Log.d(TAG, "onCreate")
+    }
+}
